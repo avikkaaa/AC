1a4fdff2b824265bf967fa86b6f92a75fe4096cc
avikkaaa <avikaar1176@gmail.com>
2026-09-15 13:28:23 +0530

    Bump PWA cache for landing image hardfix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='artisan-connect-pwa-v18';
-const APP_SHELL=['/','/manifest.webmanifest?v=18','/pwa-192.png?v=18','/pwa-512.png?v=18'];
+const CACHE='artisan-connect-pwa-v19';
+const APP_SHELL=['/','/manifest.webmanifest?v=19','/pwa-192.png?v=19','/pwa-512.png?v=19'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==self.location.origin)return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('/',copy)).catch(()=>{});return response}).catch(()=>caches.match('/')));return}event.respondWith(fetch(event.request).then(response=>{if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{})}return response}).catch(()=>caches.match(event.request)))})
